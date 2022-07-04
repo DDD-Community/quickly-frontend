c7e395bac817dfc1103a8cce9dee31547eed9d03
nimuseel <l08c0739@gmail.com>
2022-07-04 22:57:54 +0900

    refactor: Typography 적용 방식 변경 및 pretendard medium, bold font 추가

---
+++ src/components/lds/typography.tsx
@@ -0,0 +1,91 @@
+import { Text, TextStyle } from 'react-native';
+import React, { ReactNode } from 'react';
+
+interface TextProps {
+  children: ReactNode;
+  style?: TextStyle;
+  weight?: 'Regular' | 'Medium' | 'Bold';
+}
+
+export const DefaultText = ({ children, style, weight }: TextProps) => {
+  return (
+    <Text
+      allowFontScaling={false}
+      style={{ fontFamily: `Pretendard-${weight}`, ...style }}
+    >
+      {children}
+    </Text>
+  );
+};
+
+export const Headline1 = ({ children, style }: TextProps) => {
+  return (
+    <DefaultText weight="Bold" style={{ fontSize: 24, ...style }}>
+      {children}
+    </DefaultText>
+  );
+};
+
+export const Headline2 = ({ children, style }: TextProps) => {
+  return (
+    <DefaultText weight="Bold" style={{ fontSize: 22, ...style }}>
+      {children}
+    </DefaultText>
+  );
+};
+
+export const Headline3 = ({ children, style }: TextProps) => {
+  return (
+    <DefaultText weight="Bold" style={{ fontSize: 20, ...style }}>
+      {children}
+    </DefaultText>
+  );
+};
+
+export const Subhead1 = ({ children, style }: TextProps) => {
+  return (
+    <DefaultText weight="Bold" style={{ fontSize: 18, ...style }}>
+      {children}
+    </DefaultText>
+  );
+};
+
+export const Subhead2 = ({ children, style }: TextProps) => {
+  return (
+    <DefaultText weight="Medium" style={{ fontSize: 18, ...style }}>
+      {children}
+    </DefaultText>
+  );
+};
+
+export const Body1 = ({ children, style }: TextProps) => {
+  return (
+    <DefaultText weight="Regular" style={{ fontSize: 16, ...style }}>
+      {children}
+    </DefaultText>
+  );
+};
+
+export const Body2 = ({ children, style }: TextProps) => {
+  return (
+    <DefaultText weight="Regular" style={{ fontSize: 14, ...style }}>
+      {children}
+    </DefaultText>
+  );
+};
+
+export const Caption1 = ({ children, style }: TextProps) => {
+  return (
+    <DefaultText weight="Regular" style={{ fontSize: 13, ...style }}>
+      {children}
+    </DefaultText>
+  );
+};
+
+export const Caption2 = ({ children, style }: TextProps) => {
+  return (
+    <DefaultText weight="Regular" style={{ fontSize: 12, ...style }}>
+      {children}
+    </DefaultText>
+  );
+};
